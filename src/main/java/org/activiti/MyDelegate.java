d47f884dcb86cc6b511650957f97d426af1963bb
axel_ <axel.acuna@dauphine.eu>
2019-02-03 19:13:04 +0100

    Preparation du code.

---
+++ src/main/java/org/activiti/MyDelegate.java
@@ -0,0 +1,39 @@
+package org.activiti;
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+import java.util.Map;
+
+public class MyDelegate implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        /*
+        String var = (String) execution.getVariable("input");
+        var = var.toUpperCase();
+        execution.setVariable("input", var);
+        System.out.println("");*/
+        int id = Integer.parseInt(execution.getId());
+        switch (id){
+            case 3:
+                execution.setVariable("approbation",verificationPedagogique(execution));
+                break;
+        }
+
+    }
+
+    public boolean verificationPedagogique(DelegateExecution execution){
+        Map<String,Object> mapVar = execution.getVariables();
+        for(String key : mapVar.keySet()){
+            Object val = mapVar.get(key);
+            if(val instanceof String){
+                String sVal = (String) val;
+                if(sVal.equals("")){
+                    return false;                }
+            }
+        }
+        return true;
+    }
+    public boolean verificationAdministratif(DelegateExecution execution){
+        return false;
+    }
+}
